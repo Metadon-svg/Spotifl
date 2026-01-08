e91d4a49c96a155f0769d25803e527dd52c1983a
Metadon-svg <krisevicu@gmail.com>
2026-01-08 16:41:28 +0300

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -46,8 +46,11 @@ dependencies {
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
     
-    // Дополнительные библиотеки для UI и Навигации
+    // --- ВАЖНЫЕ БИБЛИОТЕКИ ---
+    // Навигация (переключение экранов)
     implementation("androidx.navigation:navigation-compose:2.7.6")
+    // Загрузка картинок (Coil)
     implementation("io.coil-kt:coil-compose:2.5.0") 
+    // Дополнительные иконки (для плеера)
     implementation("androidx.compose.material:material-icons-extended:1.5.4")
 }
